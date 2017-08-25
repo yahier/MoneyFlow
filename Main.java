fa02d404890bc937927a51aead959125daa0a96c
yahier徐子旺 <xuziwang@gmail.com>
2017-08-25 08:58:49 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * x个人，每个100块钱。每一次，每个人都随机给一块钱给另一个人。重复n次 看排行榜
+ */
+public class Main {
+    final int exchangeTimes = 50;//重复的次数
+    final int peopleNum = 50;
+    People[] peoples = new People[peopleNum];
+
+    public static final void main(String[] args) {
+        Main main = new Main();
+        main.init();
+        main.exchange();
+        main.showResult();
+    }
+
+    private void init() {
+        for (int i = 0; i < peopleNum; i++) {
+            peoples[i] = new People(i + 1);
+        }
+    }
+
+    private void exchange() {
+        Random random = new Random();
+        for (int j = 0; j < exchangeTimes; j++) {
+            for (int i = 0; i < peopleNum; i++) {
+                peoples[i].count--;
+                //不能把钱给自己
+                int index = random.nextInt(peopleNum - 1);//lucky guy
+                if (index >= i) {
+                    peoples[index + 1].count++;
+                } else {
+                    peoples[index].count++;
+                }
+
+            }
+        }
+    }
+
+    private void showResult() {
+        Arrays.sort(peoples);
+        for (int i = 0; i < peoples.length; i++) {
+            System.out.println(peoples[i].no + ":" + peoples[i].count);
+        }
+    }
+}
